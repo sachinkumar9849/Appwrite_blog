a1edcb2214dbd03684a2b2f219acec5f06e1207f
sachinmadhwa <kjhasachin@gmail.com>
2024-02-11 09:33:41 +0545

    Appwrite authentication with authSlice store, header, and footer components is completed.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-
+import { useDispatch } from "react-redux";
+import "./App.css";
+import authService from "./appwrite/auth";
+import { useEffect, useState } from "react";
+import { login, logout } from "./store/authSlice";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-export default App
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header/>
+      <Footer/>
+      </div>
+    </div>
+  ) : null;
+};
+
+export default App;
